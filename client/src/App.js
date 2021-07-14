2c7cf1af31f089aa969eca3e7b68141ce257fa96
Roope Putila <roopeputila@gmail.com>
2021-07-14 17:24:37 +0300

    adding react

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "./snaids.png";
+import "./App.css";
+import ReactJson from 'react-json-view'
+
+
+
+function App() {
+  const [data,setData]=React.useState([]);
+  const fetchData = () => {
+    return fetch("/api")
+      .then((response) => response.json())
+      .then((data) => 
+        setData(data),
+        console.log(data));
+    }
+  // fetch('/api')
+  // .then(response => response.json())
+  // .then(data => console.log(data));
+
+  //const [data, setData] = React.useState(null);
+  React.useEffect(() => {
+    fetchData()
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <p>{!data ? "Loading..." : data}</p> */}
+        {console.log(data)}
+        <ReactJson src={data} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const {spawn} = require('child_process');
 
+
+// app.use(function(req, res, next){
+//   res.header("Access-Control-Allow-Origin", "http://localhost");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// });
 
 // const result = dotenv.config()
 // if (result.error) {
@@ -11,12 +17,32 @@ require('dotenv').config()
 
 const port = process.env.BE_PORT
 
+app.get('/python', (req, res) => {
+ 
+  var dataToSend;
+  // spawn new child process to call the python script
+  const python = spawn('python', ['test.py']);
+  // collect data from script
+  python.stdout.on('data', function (data) {
+   console.log('Pipe data from python script ...');
+   dataToSend = data.toString();
+  });
+  // in close event we are sure that stream from child process is closed
+  python.on('close', (code) => {
+  console.log(`child process close all stdio with code ${code}`);
+  // send data to browser
+  res.send(dataToSend)
+  console.log(dataToSend)
+  });
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get("/api", (req, res) => {
-  res.json({ message: "hello"});
+  res.json({ message: "i hace snaids", message2: "test"});
+  console.log("i sent data")
 });
 
 app.listen(port, () => {
